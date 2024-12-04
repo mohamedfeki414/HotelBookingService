c28350aa3f30ab630643b5b90af4901aedd8388c
feki_mohamed <medfeki33@gmail.com>
2024-12-04 13:48:22 +0100

    initial commit

---
+++ src/main/java/com/hotel/booking/service/HotelBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.booking.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBookingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotel/booking/service/HotelBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.booking.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
