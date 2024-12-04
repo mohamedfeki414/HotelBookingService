7faafecd4ee500dc24f2991b8e8b00ae2b005a49
feki_mohamed <medfeki33@gmail.com>
2024-12-04 18:20:15 +0100

    ajouter les annotations to Room

--- src/main/java/com/hotel/booking/service/model/Room.java
+++ src/main/java/com/hotel/booking/service/model/Room.java
@@ -1,59 +1,88 @@
 package com.hotel.booking.service.model;
 
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
+@Table(name = "rooms")
+@EntityListeners(AuditingEntityListener.class)
 public class Room {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String roomNumber;
-    private String type; // Single, Double, Suite
-    private Double pricePerNight;
-    private Boolean isAvailable;
-    
-    public Room(Long id, String roomNumber, String type, Double pricePerNight, Boolean isAvailable) {
-		super();
-		this.id = id;
-		this.roomNumber = roomNumber;
-		this.type = type;
-		this.pricePerNight = pricePerNight;
-		this.isAvailable = isAvailable;
-	}
-    
-    public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getRoomNumber() {
-		return roomNumber;
-	}
-	public void setRoomNumber(String roomNumber) {
-		this.roomNumber = roomNumber;
-	}
-	public String getType() {
-		return type;
-	}
-	public void setType(String type) {
-		this.type = type;
-	}
-	public Double getPricePerNight() {
-		return pricePerNight;
-	}
-	public void setPricePerNight(Double pricePerNight) {
-		this.pricePerNight = pricePerNight;
-	}
-	public Boolean getIsAvailable() {
-		return isAvailable;
-	}
-	public void setIsAvailable(Boolean isAvailable) {
-		this.isAvailable = isAvailable;
-	}
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "Le numéro de chambre ne peut pas être null")
+    @JsonProperty("roomNumber")
+    private String roomNumber;
+
+    @NotNull(message = "Le type de chambre est obligatoire")
+    @JsonProperty("type")
+    private String type;
+
+    @JsonProperty("pricePerNight")
+    private Double pricePerNight;
+
+    @JsonProperty("isAvailable")
+    private Boolean isAvailable;
+
+    // Constructeur par défaut
+    public Room() {}
+
+    // Constructeur complet
+    public Room(Long id, String roomNumber, String type, Double pricePerNight, Boolean isAvailable) {
+        this.id = id;
+        this.roomNumber = roomNumber;
+        this.type = type;
+        this.pricePerNight = pricePerNight;
+        this.isAvailable = isAvailable;
+    }
+
+    // Getters et setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Double getPricePerNight() {
+        return pricePerNight;
+    }
+
+    public void setPricePerNight(Double pricePerNight) {
+        this.pricePerNight = pricePerNight;
+    }
+
+    public Boolean getIsAvailable() {
+        return isAvailable;
+    }
+
+    public void setIsAvailable(Boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
 }
--- src/main/java/com/hotel/booking/service/web/controller/RoomController.java
+++ src/main/java/com/hotel/booking/service/web/controller/RoomController.java
@@ -5,29 +5,36 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hotel.booking.service.model.Room;
-import com.hotel.booking.service.web.dao.RoomDao;
+import com.hotel.booking.service.repository.RoomRepository;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/rooms")
+@Tag(name = "Room", description = "the product API")
 public class RoomController {
-	
-	@Autowired
-    private RoomDao roomdao;
-	
-	@GetMapping
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    // Récupérer toutes les chambres
+    @GetMapping
     public List<Room> getAllRooms() {
-        return roomdao.findAll();
-    }
-	
-	@PostMapping
-    public Room createRoom(@RequestBody Room room) {
-        return roomdao.save(room);
+        return roomRepository.findAll();
     }
 
+    // Créer une nouvelle chambre
+    @PostMapping
+    public Room createRoom(@RequestBody Room room) {
+        // Log des données reçues
+        System.out.println("Requête reçue : " + room);
+
+        // Sauvegarde de la chambre dans la base de données
+        return roomRepository.save(room);
+    }
 }
