517e5fffc9878fb06546cef26f98a33b0cf13204
feki_mohamed <medfeki33@gmail.com>
2024-12-04 16:21:35 +0100

    RoomController ajouter get et post methodes

--- src/main/java/com/hotel/booking/service/web/controller/RoomController.java
+++ src/main/java/com/hotel/booking/service/web/controller/RoomController.java
@@ -1,8 +1,33 @@
 package com.hotel.booking.service.web.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hotel.booking.service.model.Room;
+import com.hotel.booking.service.web.dao.RoomDao;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 @RestController
+@RequestMapping("/rooms")
 public class RoomController {
+	
+	@Autowired
+    private RoomDao roomdao;
+	
+	@GetMapping
+    public List<Room> getAllRooms() {
+        return roomdao.findAll();
+    }
+	
+	@PostMapping
+    public Room createRoom(@RequestBody Room room) {
+        return roomdao.save(room);
+    }
 
 }
