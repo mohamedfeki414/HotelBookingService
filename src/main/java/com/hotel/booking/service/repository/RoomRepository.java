c741828d2f6242cc29dfa4ddc75dec2399bebfe0
feki_mohamed <medfeki33@gmail.com>
2024-12-04 18:19:30 +0100

    modifier le nom de repository

--- src/main/java/com/hotel/booking/service/repository/RoomRepository.java
+++ src/main/java/com/hotel/booking/service/repository/RoomRepository.java
@@ -1,4 +1,4 @@
-package com.hotel.booking.service.web.dao;
+package com.hotel.booking.service.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.hotel.booking.service.model.Room;
 
 @Repository
-public interface RoomDao extends JpaRepository<Room,Integer> {
+public interface RoomRepository extends JpaRepository<Room,Long> {
 
 }
