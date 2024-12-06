379edc2d70081c5559d6b50a879850348de0932d
feki_mohamed <medfeki33@gmail.com>
2024-12-06 11:58:42 +0100

    ajouter delete

--- src/main/java/com/hotel/booking/service/controller/RoomController.java
+++ src/main/java/com/hotel/booking/service/controller/RoomController.java
@@ -3,7 +3,9 @@ package com.hotel.booking.service.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -39,6 +41,10 @@ public class RoomController {
    @PutMapping
      void updateRoom(@RequestBody Room room) {
 	   roomRepository.save(room);
+   }
+   @DeleteMapping("/{id}")
+   public void deleteRoom(@PathVariable Long id) {
+       roomRepository.deleteById(id);
    }
    
 }
