d5658352175a417e9aa30a6ebf870087a70f320f
feki_mohamed <medfeki33@gmail.com>
2024-12-09 13:42:18 +0100

    utuliser  @ExceptionHandler et supprimer GlobalExceptionHandler ExceptionHandler sans utuliser @ControllerAdvice.

--- src/main/java/com/hotel/booking/service/exceptions/GlobalExceptionHandler.java
+++
@@ -1,9 +0,0 @@
-package com.hotel.booking.service.exceptions;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-@ControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-
-}
