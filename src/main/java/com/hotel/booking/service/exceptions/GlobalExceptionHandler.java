de5ca8a8b860056a5b13c2817a70b3dbbaa5f539
feki_mohamed <medfeki33@gmail.com>
2024-12-18 18:41:03 +0100

    .

--- src/main/java/com/hotel/booking/service/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/hotel/booking/service/exceptions/GlobalExceptionHandler.java
@@ -2,10 +2,11 @@ package com.hotel.booking.service.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
+@ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	 @ExceptionHandler(NoSuchElementFoundException.class)
 	    @ResponseStatus(HttpStatus.NOT_FOUND)
