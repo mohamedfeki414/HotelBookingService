5322e0d85b52911538edf3d859e19c15d6b5f6d9
feki_mohamed <medfeki33@gmail.com>
2024-12-19 19:08:21 +0100

    .

--- src/main/java/com/hotel/booking/service/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/hotel/booking/service/exceptions/GlobalExceptionHandler.java
@@ -2,18 +2,30 @@ package com.hotel.booking.service.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-@RestControllerAdvice
-public class GlobalExceptionHandler {
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler  {
 
     @ExceptionHandler(InvalidInputException.class)
-    public ResponseEntity<String> handleInvalidInputException(InvalidInputException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleInvalidInputException(
+    		InvalidInputException ex
+    		) {
+        return ResponseEntity
+        		.status(HttpStatus.BAD_REQUEST)
+        		.body(ex.getMessage());
     }
 
     @ExceptionHandler(NoSuchElementFoundException.class)
-    public ResponseEntity<String> handleNoSuchElementFoundException(NoSuchElementFoundException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
-    }
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public ResponseEntity<String>handleNoSushElementFoundException(
+			NoSuchElementFoundException exception
+			){
+		return ResponseEntity
+				.status(HttpStatus.NOT_FOUND)
+				.body(exception.getMessage());
+	}
 }
